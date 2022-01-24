533a99f808e5875e2eef2e74d91da57c4b26d31f
Yash D <60755759+dhayalytcnj@users.noreply.github.com>
2022-01-24 15:01:13 -0500

    Update MainApp.scala

--- src/main/scala/MainApp.scala
+++ src/main/scala/MainApp.scala
@@ -2,7 +2,8 @@
 object MainApp {
     // this will make the command-line interface
     def main(args: Array[String]): Unit = {
-        println("---------------------------------------------\n" + "--- Welcome to Yash's Banking Application ---\n" +
+        println("---------------------------------------------\n" +
+          "--- Welcome to Yash's Banking Application ---\n" +
           "---------------------------------------------")
         println("- Select an option:\n" + "1. Login\n" + "2. Sign Up")
     }
